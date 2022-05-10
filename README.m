2c822fcde5db333cbb930b786c9fab4b72738f55
BrodyGaudel <57298219+BrodyGaudel@users.noreply.github.com>
2022-05-10 06:03:32 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,4 @@
 "# User Management with Spring Security"  
+FR: Cette application permet de gerer les utilisateurs et leurs roles avec Spring Security
+EN: This application allows to manage users and their roles with Spring Security
+------------------------------------------------------------------------------------------
